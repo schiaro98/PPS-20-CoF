df9e9e8d2183d31d627ef544b25c092c99c3b4ed
lucarossi147 <lucarossi147@studio.unibo.it>
2021-08-10 17:01:05 +0200

    added basic files

--- build.sbt
+++ build.sbt
@@ -4,3 +4,11 @@ version := "0.1"
 
 scalaVersion := "2.13.6"
 
+// https://mvnrepository.com/artifact/com.google.code.gson/gson
+libraryDependencies += "com.google.code.gson" % "gson" % "2.8.7"
+
+// spray json
+//libraryDependencies += "io.spray" %%  "spray-json" % "1.3.6"
+
+//uPickle
+//libraryDependencies += "com.lihaoyi" %% "upickle" % "0.7.1"
---
+++ src/main/scala/ReadWrite.scala
@@ -0,0 +1,22 @@
+import com.google.gson.{GsonBuilder, JsonElement, JsonStreamParser}
+
+import java.io.{ByteArrayInputStream, InputStream, InputStreamReader, Reader}
+import java.nio.charset.StandardCharsets
+
+object ReadWrite extends App {
+  val initialString = "{\"brand\":\"poop\", \"doors\": 105}{\"brand\":\"Panda\", \"doors\": 5}"
+  val is = new ByteArrayInputStream(initialString.getBytes(StandardCharsets.UTF_8));
+  val r:Reader = new InputStreamReader(is, "UTF-8")
+  val gson = new GsonBuilder().create()
+  val p = new JsonStreamParser(r)
+
+  while (p.hasNext()) {
+    val e = p.next()
+    if (e.isJsonObject()) {
+       val car = gson.fromJson(e, classOf[Car])
+      println(car)
+      /* do something useful with JSON object .. */
+    }
+    /* handle other JSON data structures */
+  }
+}
