4cdedf16caeeda094266692be835c91729b1194e
lucarossi147 <lucarossi147@studio.unibo.it>
2021-09-09 14:52:04 +0200

    minor changes

--- src/main/scala/model/Habitat.scala
+++ src/main/scala/model/Habitat.scala
@@ -6,28 +6,28 @@ trait Habitat {
   val areas: Seq[Area]
 
   def checkForOverlappingAreas(areas: Seq[Area]): Boolean = areas match {
-    case h::t => {
-      for (area <- areas.filterNot(elem => elem == h) ){
+    case h :: t =>
+      for (area <- areas.filterNot(elem => elem == h)) {
         if (h.topLeft._1 > area.topLeft._1 && h.topLeft._1 < area.bottomRight._1 && h.topLeft._2 > area.topLeft._2 && h.topLeft._2 < area.bottomRight._2 ||
           h.bottomRight._1 > area.topLeft._1 && h.bottomRight._1 < area.bottomRight._1 && h.bottomRight._2 > area.topLeft._2 && h.bottomRight._2 < area.bottomRight._2 ||
           h.topLeft._1 == area.topLeft._1 && h.topLeft._2 == area.topLeft._2 || h.bottomRight._1 > area.bottomRight._1 && h.bottomRight._2 < area.bottomRight._2) {
-           return false
+          return false
         }
       }
       checkForOverlappingAreas(t)
-    }
     case _ => true
   }
 
   def checkDimensionsOfAreas(areas: Seq[Area]): Boolean = areas match {
-    case h::t =>
+    case h :: t =>
       if (h.topLeft._1 < 0 || h.topLeft._2 < 0 ||
-      h.bottomRight._1 > this.dimensions._1 || h.bottomRight._2 > this.dimensions._2) {
-      return false
+        h.bottomRight._1 > this.dimensions._1 || h.bottomRight._2 > this.dimensions._2) {
+        return false
       }
       checkDimensionsOfAreas(t)
     case _ => true
   }
+
   require(checkForOverlappingAreas(areas), "overlapping areas")
   require(checkDimensionsOfAreas(areas), "areas don't fit in current habitat")
 }
@@ -36,13 +36,12 @@ object Habitat {
 
 
   def apply(unexpectedEvents: Probability,
-             dimensions: (Int, Int),
-             areas: Seq[Area] = Seq.empty): Habitat = new SimpleHabitat(unexpectedEvents, dimensions, areas)
+            dimensions: (Int, Int),
+            areas: Seq[Area] = Seq.empty): Habitat = SimpleHabitat(unexpectedEvents, dimensions, areas)
 
-  private class SimpleHabitat( override val unexpectedEvents: Probability,
-                               override val dimensions: (Int, Int),
-                               override val areas: Seq[Area]) extends Habitat {
-  }
+  private case class SimpleHabitat(override val unexpectedEvents: Probability,
+                                   override val dimensions: (Int, Int),
+                                   override val areas: Seq[Area]) extends Habitat
 }
 
 //creare degli enum e delle factory per avere mappe sempre diverse, o mappe statiche
--- src/test/scala/ResourceManagerTest.scala
+++ src/test/scala/ResourceManagerTest.scala
@@ -6,13 +6,13 @@ class ResourceManagerTest extends AnyFunSuite{
 
   val icon = "icon.png"
 
-  val fertileArea = Area(Fertile, (0,0), (10,10))
-  val waterArea = Area(Water, (0, 15), (15,30))
-  val rockArea = Area(Rock, (40,40), (50,50))
-  val volcanoArea = Area(Volcano, (70,70), (80,80))
-  val area = Area((60,60), (65, 65), Probability(100))
+  val fertileArea: Area = Area(Fertile, (0,0), (10,10))
+  val waterArea: Area = Area(Water, (0, 15), (15,30))
+  val rockArea: Area = Area(Rock, (40,40), (50,50))
+  val volcanoArea: Area = Area(Volcano, (70,70), (80,80))
+  val area: Area = Area((60,60), (65, 65), Probability(100))
 
-  val habitat = Habitat( Probability(1), (100, 100), Seq(fertileArea, waterArea, rockArea, volcanoArea, area))
+  val habitat: Habitat = Habitat( Probability(1), (100, 100), Seq(fertileArea, waterArea, rockArea, volcanoArea, area))
 
   test("ResourceManager grow() with no growable foods"){
     val resMan = ResourceManager(habitat)
