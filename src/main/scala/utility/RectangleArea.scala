284a9d9646831b6a9b9452198be5bbf2624a79f5
davide <davide.schiaroli2@studio.unibo.it>
2021-09-24 16:57:15 +0200

    Implemented rectangle class area

---
+++ src/main/scala/utility/RectangleArea.scala
@@ -0,0 +1,15 @@
+package utility
+
+case class RectangleArea(topLeft: Point, bottomRight: Point) {
+
+  def width: Int = bottomRight.x - topLeft.x
+  def height: Int = bottomRight.y - topLeft.y
+  def isValid: Boolean = topLeft.x < bottomRight.x && topLeft.y < bottomRight.y
+  def overlap(otherArea: RectangleArea): Boolean = {
+    if (this.topLeft.x < otherArea.bottomRight.x &&
+      this.bottomRight.x > otherArea.topLeft.x &&
+      this.topLeft.y < otherArea.bottomRight.y &&
+      this.bottomRight.y > otherArea.topLeft.y) true
+    else false
+  }
+}
