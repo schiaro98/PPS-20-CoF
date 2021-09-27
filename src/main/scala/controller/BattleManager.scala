904215302030f45ff6ac1651861445e7418d0595
davide <davide.schiaroli2@studio.unibo.it>
2021-09-27 16:32:11 +0200

    Written BattleManager tests, started first battle managing

--- src/main/scala/controller/BattleManager.scala
+++ src/main/scala/controller/BattleManager.scala
@@ -4,8 +4,47 @@ import model.Animal
 
 sealed trait BattleManager {
   val animals: Seq[Animal]
+
+  def visibleAnimals(animals: Seq[Animal]) : Seq[(Animal, Animal)]
+
+  def canSee(a1: Animal, a2: Animal) : Boolean
+
+  def battle(a1: Animal, a2: Animal) : Boolean
 }
 
 object BattleManager {
+  def apply(animals: Seq[Animal]) : BattleManager = SimpleBattleManager(animals)
+  def apply() : BattleManager = SimpleBattleManager(Seq.empty)
+}
 
+private case class SimpleBattleManager(animals: Seq[Animal]) extends BattleManager {
+
+  /**
+   * Return a sequence of tuples of every animal an animal can see
+   * @param animals sequence of animals present
+   * @return Sequence of tuples
+   */
+  override def visibleAnimals(animals: Seq[Animal]): Seq[(Animal, Animal)] = {
+    var visible: Seq[(Animal, Animal)] = Seq.empty
+    for( a <- animals; b <- animals.filterNot(animal => a == animal)) {
+      if(canSee(a,b)) visible = visible :+ (a,b)
+    }
+    visible
+  }
+
+  /**
+   * Return true if animal a1 can see animal a2
+   */
+  override def canSee(a1: Animal, a2: Animal): Boolean = a1.position.distance(a2.position) <= a1.sight
+
+  /**
+   * Execute battle/figth between two animals
+   * @param a1 animal who figth
+   * @param a2 animal who has been figthed
+   * @return true if animal a1 wins, false otherwise
+   */
+  override def battle(a1: Animal, a2: Animal): Boolean = {
+    require(canSee(a1, a2))
+    ???
+  }
 }
--- src/test/scala/BattleManagerTest.scala
+++ src/test/scala/BattleManagerTest.scala
@@ -1,3 +1,29 @@
-class BattleManagerTest {
+import controller.BattleManager
+import model.Size.Medium
+import model.{Animal, Carnivorous, Species}
+import org.scalatest.funsuite.AnyFunSuite
+import utility.Point
 
+class BattleManagerTest extends AnyFunSuite{
+
+  val a1: Animal = Carnivorous(Species("", "tiger", Medium, 10, 10), Point(0,0))
+  val a2: Animal = Carnivorous(Species("", "tiger", Medium, 10, 10), Point(1,1))
+  val a3: Animal = Carnivorous(Species("", "tiger", Medium, 10, 10), Point(2,2))
+  val unreachableAnimal: Animal = Carnivorous(Species("", "tiger", Medium, 10, 10), Point(100,100))
+  val bm: BattleManager = BattleManager()
+
+  test("Test if two animals sees each other"){
+    assert(bm.canSee(a1, a2))
+    assert(bm.canSee(a2, a1))
+    assert(!bm.canSee(a1, unreachableAnimal))
+  }
+
+  test("Test if animals that sees each other are returned"){
+    assert(bm.visibleAnimals(Seq(a1,a2,a3,unreachableAnimal)).toSet ===
+      Seq((a1,a2), (a2,a1), (a1, a3), (a2, a3), (a3,a1), (a3,a2)).toSet)
+  }
+
+  test("Test battle between animals that cannot see each other"){
+    assertThrows[IllegalArgumentException](bm.battle(a1, unreachableAnimal))
+  }
 }
