c0e81d72ce97a08e4ccfc5508447cf774680b4d9
davide <davide.schiaroli2@studio.unibo.it>
2021-09-27 16:30:48 +0200

    Refactored Point

--- src/main/scala/utility/Point.scala
+++ src/main/scala/utility/Point.scala
@@ -1,15 +1,15 @@
 package utility
 
 case class Point(x: Int, y: Int) {
-   def +(add: (Int, Int)): Point = Point(this.x + add._1, this.y + add._2)
+   def +(add: (Int, Int)): Point = Point(x + add._1, y + add._2)
 
-  def -(sub: (Int, Int)): Point = Point(this.x - sub._1, this.y - sub._2)
+  def -(sub: (Int, Int)): Point = Point(x - sub._1, y - sub._2)
 
-  def +(add: Point): Point = Point(this.x + add.x, this.y + add.y)
+  def +(add: Point): Point = Point(x + add.x, y + add.y)
 
-  def -(sub: Point): Point = Point(this.x - sub.x, this.y - sub.y)
+  def -(sub: Point): Point = Point(x - sub.x, y - sub.y)
 
-  def ==(p: Point): Boolean = this.x == p.x && this.y == p.y
+  def ==(p: Point): Boolean = x == p.x && y == p.y
 
   def distance(p: Point): Double = Math.sqrt(Math.pow(x - p.x, 2) + Math.pow(y - p.y, 2))
 }
