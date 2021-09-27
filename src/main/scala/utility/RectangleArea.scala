a2711045f0f2de69ee21c2d3f01b330df66cbf54
davide <davide.schiaroli2@studio.unibo.it>
2021-09-27 15:39:30 +0200

    Refactored rectangleAre

--- src/main/scala/utility/RectangleArea.scala
+++ src/main/scala/utility/RectangleArea.scala
@@ -6,11 +6,9 @@ case class RectangleArea(topLeft: Point, bottomRight: Point) {
   def width: Int = bottomRight.x - topLeft.x
   def height: Int = bottomRight.y - topLeft.y
   def isValid: Boolean = topLeft.x < bottomRight.x && topLeft.y < bottomRight.y
-  def overlap(otherArea: RectangleArea): Boolean = {
-    if (this.topLeft.x < otherArea.bottomRight.x &&
-      this.bottomRight.x > otherArea.topLeft.x &&
-      this.topLeft.y < otherArea.bottomRight.y &&
-      this.bottomRight.y > otherArea.topLeft.y) true
-    else false
-  }
+  def overlap(otherArea: RectangleArea): Boolean =
+      topLeft.x < otherArea.bottomRight.x &&
+      bottomRight.x > otherArea.topLeft.x &&
+      topLeft.y < otherArea.bottomRight.y &&
+      bottomRight.y > otherArea.topLeft.y
 }
