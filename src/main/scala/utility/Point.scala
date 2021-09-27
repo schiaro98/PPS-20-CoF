5ae33585e5d4d65b862c1cd86ff20db46c4113ef
lucarossi147 <lucarossi147@studio.unibo.it>
2021-09-27 15:27:58 +0200

    added methods for handeling point to point operations

--- src/main/scala/utility/Point.scala
+++ src/main/scala/utility/Point.scala
@@ -5,6 +5,10 @@ case class Point(x: Int, y: Int) {
 
   def -(sub: (Int, Int)): Point = Point(this.x - sub._1, this.y - sub._2)
 
+  def +(add: Point): Point = Point(this.x + add.x, this.y + add.y)
+
+  def -(sub: Point): Point = Point(this.x - sub.x, this.y - sub.y)
+
   def ==(p: Point): Boolean = this.x == p.x && this.y == p.y
 
   def distance(p: Point): Double = Math.sqrt(Math.pow(x - p.x, 2) + Math.pow(y - p.y, 2))
