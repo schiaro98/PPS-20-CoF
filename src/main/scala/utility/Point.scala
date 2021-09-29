40e7d285645698bc0426b5435a206314d152e8ad
davide <davide.schiaroli2@studio.unibo.it>
2021-09-29 13:49:57 +0200

    Added new method that returns random point, with adequate test

--- src/main/scala/utility/Point.scala
+++ src/main/scala/utility/Point.scala
@@ -1,7 +1,10 @@
 package utility
 
+import scala.util.Random
+
 case class Point(x: Int, y: Int) {
-   def +(add: (Int, Int)): Point = Point(x + add._1, y + add._2)
+
+  def +(add: (Int, Int)): Point = Point(x + add._1, y + add._2)
 
   def -(sub: (Int, Int)): Point = Point(x - sub._1, y - sub._2)
 
@@ -12,4 +15,12 @@ case class Point(x: Int, y: Int) {
   def ==(p: Point): Boolean = x == p.x && y == p.y
 
   def distance(p: Point): Double = Math.sqrt(Math.pow(x - p.x, 2) + Math.pow(y - p.y, 2))
+
+  def getRandomPoint(limits: (Int, Int)): Point = getRandomPoint((0,0), limits)
+
+  def getRandomPoint(start: (Int, Int), ends: (Int, Int)) : Point = {
+    require(ends._1 > start._1, "Illegal random point x")
+    require(ends._2 > start._2, "Illegal random point y")
+    Point(Random.between(start._1, ends._1), Random.between(start._2, ends._2))
+  }
 }
