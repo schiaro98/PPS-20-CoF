ae2c9fae1bbff4143b076df342a6b235a679111f
davide <davide.schiaroli2@studio.unibo.it>
2021-09-29 13:52:00 +0200

    Added method that returns a non-overlapping rectangles in 4 quadrants

--- src/main/scala/utility/RectangleArea.scala
+++ src/main/scala/utility/RectangleArea.scala
@@ -11,4 +11,45 @@ case class RectangleArea(topLeft: Point, bottomRight: Point) {
       bottomRight.x > otherArea.topLeft.x &&
       topLeft.y < otherArea.bottomRight.y &&
       bottomRight.y > otherArea.topLeft.y
+
+  def getIn4Quadrant(size: (Int, Int)) : Seq[RectangleArea] = {
+    var rectangles = Seq.empty[RectangleArea]
+    val maxX = size._1 / 4
+    val maxY = size._1 / 4
+    val centerX = maxX * 2
+    val centerY = maxY * 2
+    val endX = size._1
+    val endY = size._2
+    val centerFirstQuadrant = (maxX, maxY)
+    val centerSecondQuadrant = (maxX*3, maxY)
+    val centerThirdQuadrant = (maxX, maxY*3)
+    val centerFourthQuadrant = (maxX*3, maxY*3)
+    val center = (centerX, centerY)
+    val p = Point(0,0)
+    //Alto a sx
+    rectangles = rectangles :+ RectangleArea(
+      p.getRandomPoint((0, 0), centerFirstQuadrant),
+      p.getRandomPoint(centerFirstQuadrant, center)
+    )
+
+    //Alto a dx
+    rectangles = rectangles :+ RectangleArea(
+      p.getRandomPoint((centerX, 0), centerSecondQuadrant),
+      p.getRandomPoint(centerSecondQuadrant, (endX, centerY))
+    )
+
+    //Basso a sx
+    rectangles = rectangles :+ RectangleArea(
+      p.getRandomPoint((0, centerY), centerThirdQuadrant),
+      p.getRandomPoint(centerThirdQuadrant, (centerX, endY))
+    )
+
+    //Basso a dx
+    rectangles = rectangles :+ RectangleArea(
+      p.getRandomPoint(center, centerFourthQuadrant),
+      p.getRandomPoint(centerFourthQuadrant, size)
+    )
+
+    rectangles
+  }
 }
