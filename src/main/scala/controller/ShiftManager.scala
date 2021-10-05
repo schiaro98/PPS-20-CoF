78b632b0b17df9d65421b54873a37839c9379c53
lucarossi147 <lucarossi147@studio.unibo.it>
2021-10-05 13:25:01 +0200

    Now the ShiftManager can make animal travels in an empty habitat

--- src/main/scala/controller/ShiftManager.scala
+++ src/main/scala/controller/ShiftManager.scala
@@ -1,41 +1,46 @@
 package controller
 
-import model.Animal
+import model.{Animal, Habitat}
 import utility.{Constants, Point}
 
+import scala.collection.mutable
 import scala.util.Random
 
 sealed trait ShiftManager {
-  val animalsDestinations: Map[Animal, Option[Point]]
+  val habitat: Habitat
+  var animalsDestinations: mutable.Map[Animal, Option[Point]]
 
 
-  def walk(): Seq[Animal]
+  def walk(): Unit
 
-  def animals: Set[Animal] = animalsDestinations.keySet
+  def animals: collection.Set[Animal] = animalsDestinations.keySet
   //va verso qualcosa che gli e' stato ritornato dal prolog, sia acqua o cibo, se non ha bisogno di nulla non ha una destinazione
 }
 
 
 object ShiftManager {
-  def apply(animalsDestinations: Map[Animal, Option[Point]]): ShiftManager = new ShiftManagerImpl(animalsDestinations)
+  def apply(habitat: Habitat, animalsDestinations: mutable.Map[Animal, Option[Point]]): ShiftManager =
+    new ShiftManagerImpl(habitat: Habitat, animalsDestinations)
 
-  private class ShiftManagerImpl(override val animalsDestinations: Map[Animal, Option[Point]]) extends ShiftManager {
+  private class ShiftManagerImpl(override val habitat: Habitat, override var animalsDestinations: mutable.Map[Animal, Option[Point]]) extends ShiftManager {
 
-    override def walk(): Seq[Animal] = {
-      var seq = scala.collection.mutable.Seq.empty[Animal]
+    override def walk(): Unit = {
+      var map = scala.collection.mutable.Map.empty[Animal, Option[Point]]
       val travelDistance: (Int, Int) = (Random.nextInt(Constants.MaxShift), Random.nextInt(Constants.MaxShift))
       for (a <- animals) {
-        println(a.name, animalsDestinations.getOrElse(a, None) )
+        println(a.name, animalsDestinations.getOrElse(a, None))
         //animal has a destination
         if (animalsDestinations(a).isDefined) {
+          println("animal has a destination")
           val dest = animalsDestinations(a).get
           if (canTravel(a.position, dest, travelDistance)) {
-            seq :+= a.shift(dest)
-          } else seq :+= a.shift(calcNewPoint(a.position, dest, travelDistance))
+            map.+= (a.shift(dest) ->None)
+          } else map += a.shift(calcNewPoint(a.position, dest, travelDistance))->Some(dest)
           //animal doesn't have a destination
-        } else seq :+= a.shift(calcRandomPoint(a.position, travelDistance))
+
+        } //else seq :+= a.shift(calcRandomPoint(a.position, travelDistance))
       }
-      seq.toSeq
+      animalsDestinations = map
     }
 
     /**
@@ -50,11 +55,26 @@ object ShiftManager {
       travelDistance._1 - distance.x.abs > 0 && travelDistance._2 - distance.y.abs > 0
     }
 
+    // TODO: now simplifing saing that there are only walkableAreas
+    
     //cambiare nome a questi 2?
-    def calcNewPoint(from: Point, to: Point, travelDistance: (Int, Int)): Point = ???
+    def calcNewPoint(from: Point, to: Point, travelDistance: (Int, Int)): Point = {
+      // TODO: fix if animal can arrive in same x or y, do not have to go further
+//   todo   do I need all the travelDistance?
+      //if not
+      
+      
+      //else
+      val px = if (to.isRight(from)) from.x + travelDistance._1 else from.x - travelDistance._1
+      val py = if (to.isUnder(from)) from.y + travelDistance._2 else from.y - travelDistance._2
+      
+      val x = if(px < 0) 0 else if (px >habitat.dimensions._1) habitat.dimensions._1 else px
+      val y = if(py <0) 0 else if (py> habitat.dimensions._2) habitat.dimensions._2 else py
+      println("new point is ", x, " ", y )
+      Point(x,y)
+    }
 
     def calcRandomPoint(from: Point, travelDistance: (Int, Int)): Point = ???
-
   }
 }
 
--- src/test/scala/ShiftManagerTest.scala
+++ src/test/scala/ShiftManagerTest.scala
@@ -1,24 +1,49 @@
 import controller.ShiftManager
 import model.Size.Medium
-import model.{Animal, Carnivorous, Herbivore, Species}
+import model.{Animal, Area, Carnivorous, EmptyHabitatType, Habitat, Herbivore, Probability, Species}
 import org.scalatest.funsuite.AnyFunSuite
 import utility.Point
 
+import scala.collection.mutable
+
 class ShiftManagerTest extends AnyFunSuite{
+
+  val ourHabitat = Habitat(Probability(0))
 
   val path = "res/images/tiger-icon.jpg"
   val c: Animal = Carnivorous(Species(path, "tiger", Medium, 10, 10), Point(15,15))
   val h: Animal = Herbivore(Species(path, "elephant", Medium, 10, 10),  Point(40,40))
 
   test("Create ShiftManager"){
-    val sm: ShiftManager = ShiftManager(Map(c->Some(Point(55,100)), h->None))
+    val sm: ShiftManager = ShiftManager(ourHabitat, mutable.Map(c->Some(Point(55,100)), h->None))
     assert(sm.animals.size == 2)
   }
 
+  test("Test ShiftManager in an empty habitat"){
+    val habitat = Habitat(EmptyHabitatType,Probability(0),(500,500), Seq.empty[Area])
+    val sm: ShiftManager = ShiftManager(habitat, mutable.Map(c->Some(Point(55,100)), h->Some(Point(0,0))))
+    sm.walk()
+    sm.walk()
+    sm.walk()
+    sm.walk()
+    sm.walk()
+    sm.walk()
+    sm.walk()
+    sm.walk()
+    sm.walk()
+    sm.walk()
+    sm.walk()
+    sm.walk()
+    sm.walk()
+    sm.walk()
+    assert(!sm.animalsDestinations.contains(c))
+    assert(!sm.animalsDestinations.contains(h))
+  }
+
   test("Testing walk in ShiftManager"){
-    val sm: ShiftManager = ShiftManager(Map(c->Some(Point(55,100)), h->None))
+    val sm: ShiftManager = ShiftManager(ourHabitat, mutable.Map(c->Some(Point(55,100)), h->None))
     val fuck = sm.walk()
-    println(fuck.foreach(println))
+//    println(fuck.foreach(println))
   }
 
 }
