377b6a989c49e6f42c9b11b4706da3988e8d33e0
lucarossi147 <lucarossi147@studio.unibo.it>
2021-10-05 17:48:28 +0200

    changed hardcoded value

--- src/test/scala/ShiftManagerTest.scala
+++ src/test/scala/ShiftManagerTest.scala
@@ -8,13 +8,15 @@ import scala.util.Random
 
 class ShiftManagerTest extends AnyFunSuite{
 
-  val ourHabitat = Habitat(Probability(0))
+  val ourHabitat: Habitat = Habitat(Probability(0))
 
   val path = "res/images/tiger-icon.jpg"
   val tiger: Animal = Carnivorous(Species(path, "tiger", Medium, 10, 10), Point(15,15))
   val elephant: Animal = Herbivore(Species(path, "elephant", Medium, 10, 10),  Point(40,40))
   val dog: Animal = Carnivorous(Species(path, "dog", Medium, 20,20), Point(200,200))
   val cat: Animal = Carnivorous(Species(path, "cat", Small, 15,10), Point(100,20))
+  val MaxX = 500
+  val MaxY = 500
 
   test("Create ShiftManager"){
     val sm: ShiftManager = ShiftManager(ourHabitat, Map(tiger->Some(Point(55,100)), elephant->None))
@@ -33,12 +35,12 @@ class ShiftManagerTest extends AnyFunSuite{
 
   test("Multiple animals arrive at different random destinations in empty habitat"){
     val r = (x:Int) => Random.nextInt(x)
-    val dest1 = Point(r(500),r(500))
-    val dest2 = Point(r(500),r(500))
-    val dest3 = Point(r(500),r(500))
-    val dest4 = Point(r(500),r(500))
+    val dest1 = Point(r(MaxX),r(MaxY))
+    val dest2 = Point(r(MaxX),r(MaxY))
+    val dest3 = Point(r(MaxX),r(MaxY))
+    val dest4 = Point(r(MaxX),r(MaxY))
     val destinations = Set(dest1,dest2,dest3,dest4)
-    val habitat = Habitat(EmptyHabitatType,Probability(0),(500,500), Seq.empty[Area])
+    val habitat = Habitat(EmptyHabitatType,Probability(0),(MaxX, MaxY), Seq.empty[Area])
     val sm: ShiftManager = ShiftManager(habitat,
       Map(tiger->Some(dest1),
         elephant->Some(dest2),
@@ -68,11 +70,5 @@ class ShiftManagerTest extends AnyFunSuite{
     }
     assert(sm.animals.count(a => a.position == dest) == sm.animals.size)
   }
-
-//  test("Testing walk in ShiftManager"){
-//    val sm: ShiftManager = ShiftManager(ourHabitat, Map(c->Some(Point(55,100)), h->None))
-//    val fuck = sm.walk()
-//    println(fuck.foreach(println))
-//  }
 
 }
