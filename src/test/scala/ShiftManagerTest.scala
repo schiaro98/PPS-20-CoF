def3e52fd5ddf070720a0856e7ff0d5812d45aab
lucarossi147 <lucarossi147@studio.unibo.it>
2021-10-05 16:44:44 +0200

    added more tests

--- src/test/scala/ShiftManagerTest.scala
+++ src/test/scala/ShiftManagerTest.scala
@@ -1,49 +1,78 @@
 import controller.ShiftManager
-import model.Size.Medium
+import model.Size.{Medium, Small}
 import model.{Animal, Area, Carnivorous, EmptyHabitatType, Habitat, Herbivore, Probability, Species}
 import org.scalatest.funsuite.AnyFunSuite
 import utility.Point
 
-import scala.collection.mutable
+import scala.util.Random
 
 class ShiftManagerTest extends AnyFunSuite{
 
   val ourHabitat = Habitat(Probability(0))
 
   val path = "res/images/tiger-icon.jpg"
-  val c: Animal = Carnivorous(Species(path, "tiger", Medium, 10, 10), Point(15,15))
-  val h: Animal = Herbivore(Species(path, "elephant", Medium, 10, 10),  Point(40,40))
+  val tiger: Animal = Carnivorous(Species(path, "tiger", Medium, 10, 10), Point(15,15))
+  val elephant: Animal = Herbivore(Species(path, "elephant", Medium, 10, 10),  Point(40,40))
+  val dog: Animal = Carnivorous(Species(path, "dog", Medium, 20,20), Point(200,200))
+  val cat: Animal = Carnivorous(Species(path, "cat", Small, 15,10), Point(100,20))
 
   test("Create ShiftManager"){
-    val sm: ShiftManager = ShiftManager(ourHabitat, mutable.Map(c->Some(Point(55,100)), h->None))
+    val sm: ShiftManager = ShiftManager(ourHabitat, Map(tiger->Some(Point(55,100)), elephant->None))
     assert(sm.animals.size == 2)
   }
 
-  test("Test ShiftManager in an empty habitat"){
+  test("Single animal arrives at destination in empty habitat"){
+    val dest = Point(55,100)
     val habitat = Habitat(EmptyHabitatType,Probability(0),(500,500), Seq.empty[Area])
-    val sm: ShiftManager = ShiftManager(habitat, mutable.Map(c->Some(Point(55,100)), h->Some(Point(0,0))))
-    sm.walk()
-    sm.walk()
-    sm.walk()
-    sm.walk()
-    sm.walk()
-    sm.walk()
-    sm.walk()
-    sm.walk()
-    sm.walk()
-    sm.walk()
-    sm.walk()
-    sm.walk()
-    sm.walk()
-    sm.walk()
-    assert(!sm.animalsDestinations.contains(c))
-    assert(!sm.animalsDestinations.contains(h))
+    val sm: ShiftManager = ShiftManager(habitat, Map(tiger->Some(dest)))
+    for(i <- 0 to 40){
+      sm.walk()
+    }
+    assert(sm.animals.count(a => a.position == dest) == 1)
   }
 
-  test("Testing walk in ShiftManager"){
-    val sm: ShiftManager = ShiftManager(ourHabitat, mutable.Map(c->Some(Point(55,100)), h->None))
-    val fuck = sm.walk()
-//    println(fuck.foreach(println))
+  test("Multiple animals arrive at different random destinations in empty habitat"){
+    val r = (x:Int) => Random.nextInt(x)
+    val dest1 = Point(r(500),r(500))
+    val dest2 = Point(r(500),r(500))
+    val dest3 = Point(r(500),r(500))
+    val dest4 = Point(r(500),r(500))
+    val destinations = Set(dest1,dest2,dest3,dest4)
+    val habitat = Habitat(EmptyHabitatType,Probability(0),(500,500), Seq.empty[Area])
+    val sm: ShiftManager = ShiftManager(habitat,
+      Map(tiger->Some(dest1),
+        elephant->Some(dest2),
+        dog->Some(dest3),
+        cat->Some(dest4)
+      ))
+      //100 iterations should be enough
+    for(i <- 0 to 100){
+      sm.walk()
+    }
+    assert(sm.animals.count(a => destinations.contains(a.position)) == sm.animals.size)
   }
+
+  test("Multiple animals arrive at the same random destination in empty habitat"){
+    val r = (x:Int) => Random.nextInt(x)
+    val dest = Point(r(500),r(500))
+    val habitat = Habitat(EmptyHabitatType,Probability(0),(500,500), Seq.empty[Area])
+    val sm: ShiftManager = ShiftManager(habitat,
+      Map(tiger->Some(dest),
+        elephant->Some(dest),
+        dog->Some(dest),
+        cat->Some(dest)
+      ))
+    //100 iterations should be enough
+    for(i <- 0 to 100){
+      sm.walk()
+    }
+    assert(sm.animals.count(a => a.position == dest) == sm.animals.size)
+  }
+
+//  test("Testing walk in ShiftManager"){
+//    val sm: ShiftManager = ShiftManager(ourHabitat, Map(c->Some(Point(55,100)), h->None))
+//    val fuck = sm.walk()
+//    println(fuck.foreach(println))
+//  }
 
 }
