52a446b556cb885b109dad272fa5d1e9039d2ca8
Simone Luzi <simone.luzi@studio.unibo.it>
2021-10-13 19:00:53 +0200

    added some scaladoc

--- src/main/scala/controller/GameLoop.scala
+++ src/main/scala/controller/GameLoop.scala
@@ -9,11 +9,12 @@ import java.awt.Color
 import scala.util.Random
 
 /**
- * Runnable used to start the simulation, it contains the game loop which must be executed in a new thread
- * @param species the Species with the number of animals to create at the beginning of the simulation
- * @param habitat the Habitat where the simulation takes place
+ * Runnable used to start the simulation, it contains the game loop which must be executed in a new thread.
+ *
+ * @param species the Species with the number of animals to create at the beginning of the simulation.
+ * @param habitat the Habitat where the simulation takes place.
  */
-case class GameLoop(species : Map[Species, Int], habitat: Habitat) extends Runnable {
+case class GameLoop(species: Map[Species, Int], habitat: Habitat) extends Runnable {
 
   var animalsAndRectangles: Map[Animal, Rectangle] = Map.empty[Animal, Rectangle] //TODO lasciamo var?
   val animalsInMap: Seq[Animal] = generateInitialAnimals()
@@ -21,15 +22,15 @@ case class GameLoop(species : Map[Species, Int], habitat: Habitat) extends Runna
   val battleManager: BattleManager = BattleManager(animalsInMap)
   val shiftManager: ShiftManager = ShiftManager(habitat, Map.empty[Animal, Point])
 
-//  val battleManager: BattleManager = BattleManager(getAnimalsInMap)
-
   override def run(): Unit = {
     val shapePanel = new ShapePanel(habitat.dimensions._1, habitat.dimensions._2)
-    val simulationGui = new SimulationGui(habitat, shapePanel) {top.visible = true }
+    val simulationGui = new SimulationGui(habitat, shapePanel) {
+      top.visible = true
+    }
     simulationGui.updatePanel(animalsAndRectangles)
 
     var previous: Long = System.currentTimeMillis()
-    while(animalsInMap.lengthIs > 1) { //TODO pausa come fermare il gioco senza sprecare cpu?
+    while (animalsInMap.lengthIs > 1) { //TODO pausa come fermare il gioco senza sprecare cpu?
       val current: Long = System.currentTimeMillis()
       val elapsed: Double = (current - previous) / Constants.MillisToSec
 
@@ -38,12 +39,15 @@ case class GameLoop(species : Map[Species, Int], habitat: Habitat) extends Runna
       //Non credo sia proprio lo shift a dover vedere sta cosa ma vabbe
       battleManager.calculateBattles()
       //Calcolo eventi inaspettati
+      //crescita casuale dei vegetali
 
 
-      //---- solo per vedere che la gui cambia
-      animalsAndRectangles = Map.empty
-      generateInitialAnimals()
-      //---- solo per vedere che la gui cambia
+      //---- solo per vedere che la gui cambia----|
+      animalsAndRectangles = Map.empty //         |
+      generateInitialAnimals() //                 |
+      //---- solo per vedere che la gui cambia----|
+
+
       simulationGui.updatePanel(animalsAndRectangles)
       waitForNextFrame(current)
       previous = current
@@ -52,6 +56,11 @@ case class GameLoop(species : Map[Species, Int], habitat: Habitat) extends Runna
     //TODO mostra la gui con il riassunto
   }
 
+  /**
+   * Method that permit to wait the current time to render the following frame.
+   *
+   * @param current time at the beginning of current frame.
+   */
   def waitForNextFrame(current: Long): Unit = {
     val dt = System.currentTimeMillis() - current
     println("Time elapsed for the computation: " + dt)
@@ -62,51 +71,69 @@ case class GameLoop(species : Map[Species, Int], habitat: Habitat) extends Runna
 
   def generateInitialFood(): Seq[FoodInstance] = {
     var food = Seq.empty[FoodInstance]
-//    habitat.areas.filter(a => a.isInstanceOf[FertileAreaGrowFood])
+    //    habitat.areas.filter(a => a.isInstanceOf[FertileAreaGrowFood])
     food
   }
 
+  /**
+   * Method to create a number of animals for each species equal to the one in the Map.
+   *
+   * @return the created animals.
+   */
   def generateInitialAnimals(): Seq[Animal] = {
     //TODO fare in modo più funzionale (for yield ad esempio)
     var animals = Seq.empty[Animal]
     species foreach (s => {
       val color = new Color(Random.nextFloat(), Random.nextFloat(), Random.nextFloat()) //TODO cambia colore hardcoded
       for (_ <- 1 to s._2) {
-        val (x,y) = placeAnimal(s._1)
+        val (x, y) = placeAnimal(s._1)
         val animal = Animal(s = s._1, x)
         animals = animals :+ animal
-        animalsAndRectangles += (animal -> new Rectangle(x,y,color))
+        animalsAndRectangles += (animal -> new Rectangle(x, y, color))
       }
     })
     animals
   }
 
   /**
-   * Method used to obtain a random permissible point to create an animal of a certain species
-   * @param species the Species of the animal
-   * @return the Point (top left) to create the animal and the Point (bottom right) used to draw the rectangle
+   * Method used to obtain a random permissible point to create an animal of a certain species.
+   *
+   * @param species the Species of the animal.
+   * @return the Point (top left) to create the animal and the Point (bottom right) used to draw the rectangle.
    */
-  def placeAnimal(species: Species): (Point,Point) = {
+  def placeAnimal(species: Species): (Point, Point) = {
     val (width, height) = habitat.dimensions
-    val areas = habitat.areas
     val size = species.size match {
       case Big => Constants.PixelForBig
       case Medium => Constants.PixelForMedium
       case Small => Constants.PixelForSmall
     }
-
     var x = Random.nextInt(width - size)
     var y = Random.nextInt(height - size)
-    while (isNotPlaceable(Point(x,y),areas) || isNotPlaceable(Point(x+size,y+size),areas)
-      || isNotPlaceable(Point(x+size,y),areas) || isNotPlaceable(Point(x,y+size),areas)) {
+    while (areNotPlaceable(Seq(Point(x, y), Point(x + size, y + size), Point(x + size, y), Point(x, y + size)))) {
       x = Random.nextInt(width - size)
       y = Random.nextInt(height - size)
     }
-    (Point(x,y), Point(x+size,y+size))
+    (Point(x, y), Point(x + size, y + size))
   }
 
-  def isNotPlaceable(p: Point, areas: Seq[Area]): Boolean = {
-    Constants.NonWalkableArea.contains(areas.find(a => a.area.contains(p)).getOrElse(return false).areaType)
+
+  /**
+   * Check if a sequence of point is in a non-walkable area.
+   *
+   * @param points the points whose positions are to be checked.
+   * @return true if at least one point is not placeable because was in a non-walkable area, otherwise true.
+   */
+  def areNotPlaceable(points: Seq[Point]): Boolean = points.exists(p => isNotPlaceable(p))
+
+  /**
+   * Check if a point is in a non-walkable area.
+   *
+   * @param p the point whose position is to be checked.
+   * @return true if the point is not placeable because was in a non-walkable area, otherwise true.
+   */
+  def isNotPlaceable(p: Point): Boolean = {
+    Constants.NonWalkableArea.contains(habitat.areas.find(a => a.area.contains(p)).getOrElse(return false).areaType)
   }
 }
 
