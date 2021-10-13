374a720ee0e5f05476dc7ccab0924073461329b8
schiaro98 <davide.schiaroli2@studio.unibo.it>
2021-10-13 19:44:11 +0200

    Refactored a _

--- src/main/scala/view/ShapePanel.scala
+++ src/main/scala/view/ShapePanel.scala
@@ -58,7 +58,7 @@ class ShapePanel(val width: Int, val height: Int) extends Panel {
       val mouseListener: MouseMotionListener = new MouseMotionListener {
         override def mouseDragged(e: MouseEvent): Unit = {}
         override def mouseMoved(e: MouseEvent): Unit = {
-          if (Point(e.getX, e.getY).isInside(v._2.topLeft, v._2.bottomRigth)) {
+          if (Point(e.getX, e.getY).isInside(rectangle.topLeft, rectangle.bottomRigth)) {
             p.setVisible(true)
           } else {
             p.setVisible(false)
