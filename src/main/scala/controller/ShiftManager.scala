18d907a0173f5223441acdf874e1f6fd3313d2e3
lucarossi147 <lucarossi147@studio.unibo.it>
2021-10-13 11:12:04 +0200

    now ShiftManager works but not 100% of the time

--- src/main/scala/controller/ShiftManager.scala
+++ src/main/scala/controller/ShiftManager.scala
@@ -8,84 +8,66 @@ import scala.util.Random
 
 sealed trait ShiftManager {
   val habitat: Habitat
-  var animalsDestinations: Map[Animal, Seq[Point]]
+  var animalsDestinations: Map[Animal, Point]
 
   def walk(): Unit
 
-  def animals: collection.Set[Animal] = animalsDestinations.keySet
+  def animals: collection.Set[Animal]
   //va verso qualcosa che gli e' stato ritornato dal prolog, sia acqua o cibo, se non ha bisogno di nulla non ha una destinazione
 }
 
 
 object ShiftManager {
   def apply(habitat: Habitat, animalDestinations: (Animal, Point)*): ShiftManager =
-    new ShiftManagerImpl(habitat, animalDestinations.map(c => (c._1, Seq(c._2))).toMap)
+    new ShiftManagerImpl(habitat, animalDestinations.toMap)
 
-  def apply(habitat: Habitat, animalsDestinations: Map[Animal, Seq[Point]]): ShiftManager =
+  def apply(habitat: Habitat, animalsDestinations: Map[Animal, Point]): ShiftManager =
     new ShiftManagerImpl(habitat: Habitat, animalsDestinations)
 
 
-  private class ShiftManagerImpl(override val habitat: Habitat, override var animalsDestinations: Map[Animal, Seq[Point]]) extends ShiftManager {
+  private class ShiftManagerImpl(override val habitat: Habitat, override var animalsDestinations: Map[Animal, Point]) extends ShiftManager {
 
     val nonWalkableAreas: Seq[Area] = habitat.areas.filterNot(a => a.areaType == Fertile)
-
-    animals.foreach(animal => require(nonWalkableAreas.count(a => a.contains(animal.position)) == 0))
-
     val randShift: Unit => Int = _ => Random.between(Constants.MinShift, Constants.MaxShift)
+    private def isLegal(p: Point): Boolean = !nonWalkableAreas.exists(a => a.contains(p))
+    animalsDestinations.keySet.foreach(animal => require(nonWalkableAreas.count(a => a.contains(animal.position)) == 0))
+    var mySupportAnimalsDestinations: Map[Animal, Seq[Point]] = initWalks(animalsDestinations.keySet.toSeq)
+    override def animals: collection.Set[Animal] = mySupportAnimalsDestinations.keySet
 
-    // TODO: try to do it with for yield
-    //if I can't go to the first point create another, go to that point and when arrived go to the first one
+
     override def walk(): Unit = {
-      val map = scala.collection.mutable.Map.empty[Animal, Seq[Point]]
-      for (a <- animals) {
-        val travelDistance: (Int, Int) = (randShift(), randShift())
-        //animal has a destination
-        if (animalsDestinations(a).nonEmpty) {
-          val dest = animalsDestinations(a)
-          //animal can arrive to destination in this iteration
-          if (canTravel(a.position, dest.head, travelDistance)) {
-            println(a.name, " is arriving")
-            map += (a.shift(dest.head) -> dest.tail)
-            //animal has to calc a new point closer to destination
-          } else {
-            val destinations = tryWalk(a.position, dest, travelDistance)
-            if (!canTravel(a.position, destinations.head, travelDistance)) {
-              println("fok")
-            }
-            map += a.shift(destinations.head) -> destinations.tail
-          }
-          //animal doesn't have a destination
-          // TODO: implement this
-        } else map += (a -> Seq.empty)
+      @tailrec
+      def recWalk(animals: Seq[Animal], updatedAnimalsDestinations: Map[Animal, Seq[Point]] = Map.empty[Animal, Seq[Point]]): Map[Animal, Seq[Point]] = animals match {
+        case a +: others =>
+          if (mySupportAnimalsDestinations(a).nonEmpty) {
+            recWalk(others, updatedAnimalsDestinations + (a.shift(mySupportAnimalsDestinations(a).head) -> mySupportAnimalsDestinations(a).tail))
+          } else recWalk(others, updatedAnimalsDestinations)
+        case _ => updatedAnimalsDestinations
       }
-      animalsDestinations = map.toMap
+      mySupportAnimalsDestinations = recWalk(mySupportAnimalsDestinations.keySet.toSeq)
     }
 
     @tailrec
-    private def tryWalk(position: Point, dest: Seq[Point], travelDistance: (Int, Int)): Seq[Point] = {
-      val nextP = calcNextDest(position, dest.head, travelDistance)
-      println(isLegal(nextP))
-      if (isLegal(nextP)) nextP +: dest else tryWalk(position, calcLegalRandomPointInRectangle(dest.head, position) +: dest, travelDistance)
+    private def initWalks(animals: Seq[Animal], map: Map[Animal, Seq[Point]] = Map.empty): Map[Animal, Seq[Point]] = animals match {
+      case h +: t => initWalks(t, Map(h -> createPath(h.position, animalsDestinations(h))))
+      case _ => map
     }
 
-    /**
-     *
-     * @param from           Point where the animal is
-     * @param to             Point in which the animal wants to go
-     * @param travelDistance distance that the animal can travel in this iteration
-     * @return true if the animal can make it to the destination
-     */
-    private def canTravel(from: Point, to: Point, travelDistance: (Int, Int)): Boolean = {
-      val distance = to - from
-      travelDistance._1 - distance.x.abs >= 0 && travelDistance._2 - distance.y.abs >= 0
+    @tailrec
+    private def createPath(from: Point, dest: Point, path: Seq[Point] = Seq.empty): Seq[Point] = path match {
+      case _ if from == dest => path.reverse
+      case _ =>
+        val travelDistance = (randShift(), randShift())
+        //        println( "traveldistance ", travelDistance)
+        val topLeft = makeInBounds(from.x - travelDistance._1, from.y - travelDistance._2)
+        val bottomRight = makeInBounds(from.x + travelDistance._1, from.y + travelDistance._2)
+        val legalPoints = for (x <- topLeft.x to bottomRight.x;
+                               y <- topLeft.y to bottomRight.y if isLegal(Point(x, y)))
+        yield Point(x, y)
+        val closerPoint = findCloserPoint(legalPoints, dest)
+        createPath(closerPoint, dest, closerPoint +: path)
     }
 
-    private def calcNextDest(from: Point, to: Point, travelDistance: (Int, Int)): Point = {
-      //calculating potential points
-      val x = if (to.isRight(from)) from.x + travelDistance._1 else from.x - travelDistance._1
-      val y = if (to.isUnder(from)) from.y + travelDistance._2 else from.y - travelDistance._2
-      makeInBounds(x, y)
-    }
 
     /**
      * Make coordinates be inside of the habitat
@@ -101,41 +83,8 @@ object ShiftManager {
     }
 
     @tailrec
-    private def calcLegalRandomPointInRectangle(p1: Point, p2: Point, tries: Int = 0): Point = {
-      val x = if (p1.x != p2.x) Random.between(Ordering.Int.min(p1.x, p2.x), Ordering.Int.max(p1.x, p2.x)) else p1.x
-      val y = if (p1.y != p2.y) Random.between(Ordering.Int.min(p1.y, p2.y), Ordering.Int.max(p1.y, p2.y)) else p2.y
-      val p = Point(x, y)
-      if (isLegal(p)) p else calcLegalRandomPointInRectangle(p1, p2)
-    }
-
-    private def isLegal(p: Point): Boolean = !nonWalkableAreas.exists(a => a.contains(p))
-
-
-    @tailrec
-    private def initWalks(animals:Seq[Animal], map: Map[Animal, Seq[Point]] = Map.empty ): Map[Animal, Seq[Point]] = animals match {
-      case h::t => initWalks(t, Map(h -> createPath(h.position, animalsDestinations(h).head)))
-      case _ => map
-    }
-
-
-    @tailrec
-    private def createPath(from: Point, dest:Point, path: Seq[Point] = Seq.empty):Seq[Point] = path match {
-      case h::_ if h == dest => path.reverse
-      case _ =>
-        val travelDistance = (randShift(), randShift())
-        val topLeft = makeInBounds(from.x - travelDistance._1, from.y - travelDistance._2)
-        val bottomRight = makeInBounds(from.x + travelDistance._1, from.y + travelDistance._2)
-        val legalPoints = for (x <- topLeft.x to bottomRight.x;
-                               y <- topLeft.y to bottomRight.y if isLegal(Point(x, y)) && from != Point(x, y))
-        yield Point(x, y)
-        val closerPoint = findCloserPoint(legalPoints, dest)
-        createPath(closerPoint,dest, closerPoint+:path)
-     }
-
-
-    @tailrec
-    private def findCloserPoint(points: Seq[Point], dest: Point, closestP: Option[Point] = None, minDist:Double = Double.MaxValue):Point = points match {
-      case h::t =>
+    private def findCloserPoint(points: Seq[Point], dest: Point, closestP: Option[Point] = None, minDist: Double = Double.MaxValue): Point = points match {
+      case h +: t =>
         val d = h.distance(dest)
         if (d < minDist) findCloserPoint(t, dest, Some(h), d) else findCloserPoint(t, dest, closestP, minDist)
       case _ => closestP.getOrElse(throw new RuntimeException("LIKE IN LIFE, THERE ISN'T A POINT"))
--- src/test/scala/ShiftManagerTest.scala
+++ src/test/scala/ShiftManagerTest.scala
@@ -20,20 +20,20 @@ class ShiftManagerTest extends AnyFunSuite {
   val MaxY = 500
 
   test("Create ShiftManager") {
-    val sm: ShiftManager = ShiftManager(ourHabitat, Map(tiger -> Seq(Point(55, 100)), elephant -> Seq.empty))
+    val sm: ShiftManager = ShiftManager(ourHabitat, Map(tiger -> Point(55, 100)))
     assert(sm.animals.size == 2)
   }
 
   test("Single animal arrives at destination in empty habitat") {
     val dest = Point(55, 100)
     val habitat = Habitat(EmptyHabitatType, Probability(0), (500, 500), Seq.empty[Area])
-    val sm: ShiftManager = ShiftManager(habitat, Map(tiger -> Seq(dest)))
+    val sm: ShiftManager = ShiftManager(habitat, Map(tiger -> dest))
     var i = 0
     while (sm.animals.count(a => a.position == dest) != 1){
       sm.walk()
       i+=1
     }
-    println(i)
+//    println(i)
     assert(sm.animals.count(a => a.position == dest) == 1)
   }
 
@@ -46,10 +46,10 @@ class ShiftManagerTest extends AnyFunSuite {
     val destinations = Set(dest1, dest2, dest3, dest4)
     val habitat = Habitat(EmptyHabitatType, Probability(0), (MaxX, MaxY), Seq.empty[Area])
     val sm: ShiftManager = ShiftManager(habitat,
-      Map(tiger -> Seq(dest1),
-        elephant -> Seq(dest2),
-        dog -> Seq(dest3),
-        cat -> Seq(dest4)
+      Map(tiger -> dest1,
+        elephant -> dest2,
+        dog -> dest3,
+        cat -> dest4
       ))
     var i = 0
     while(sm.animals.count(a => destinations.contains(a.position)) != sm.animals.size) {
@@ -63,12 +63,7 @@ class ShiftManagerTest extends AnyFunSuite {
     val r = (x: Int) => Random.nextInt(x)
     val dest = Point(r(500), r(500))
     val habitat = Habitat(EmptyHabitatType, Probability(0), (500, 500), Seq.empty[Area])
-    val sm: ShiftManager = ShiftManager(habitat,
-      Map(tiger -> Seq(dest),
-        elephant -> Seq(dest),
-        dog -> Seq(dest),
-        cat -> Seq(dest)
-      ))
+    val sm: ShiftManager = ShiftManager(habitat, (tiger, dest),(elephant, dest), (dog,dest), (cat,dest))
     var i = 0
     while(sm.animals.count(a => a.position == dest) != sm.animals.size) {
       sm.walk()
@@ -77,26 +72,33 @@ class ShiftManagerTest extends AnyFunSuite {
     assert(sm.animals.count(a => a.position == dest) == sm.animals.size)
   }
 
-//  test("Multiple animals arrive at different random destinations in ours habitat") {
-//    val dest1 = getPointInFertileAreas(ourHabitat)
-//    val dest2 = getPointInFertileAreas(ourHabitat)
-//    val dest3 = getPointInFertileAreas(ourHabitat)
-//    val dest4 = getPointInFertileAreas(ourHabitat)
-//    val destinations = Set(dest1, dest2, dest3, dest4)
-//    val sm: ShiftManager = ShiftManager(ourHabitat,
-//      Map(tiger -> Seq(dest1),
-//        elephant -> Seq(dest2),
-//        dog -> Seq(dest3),
-//        cat -> Seq(dest4)
-//      ))
-//    var i = 0
-//    while(sm.animals.count(a => destinations.contains(a.position)) != sm.animals.size){
-//      i += 1
-//      sm.walk()
-//      sm.animals.foreach(animal => require(ourHabitat.areas.filterNot(a => a.areaType == Fertile).count(a => a.contains(animal.position)) == 0))
-//    }
-//    assert(sm.animals.count(a => destinations.contains(a.position)) == sm.animals.size)
-//  }
+  test("One animal arrives at different random destination in ours habitat") {
+    val d = getLegalRandomPoint(ourHabitat)
+    val sm: ShiftManager = ShiftManager(ourHabitat,(tiger, d))
+    var i = 0
+    while(sm.animals.count(a => a.position == d) != sm.animals.size){
+      i += 1
+      sm.walk()
+      sm.animals.foreach(animal => require(ourHabitat.areas.filterNot(a => a.areaType == Fertile).count(a => a.contains(animal.position)) == 0))
+    }
+    assert(sm.animals.count(a => a.position == d) == sm.animals.size)
+  }
+
+  test("Multiple animals arrive at different random destinations in ours habitat") {
+    val dest1 = getLegalRandomPoint(ourHabitat)
+    val dest2 = getLegalRandomPoint(ourHabitat)
+    val dest3 = getLegalRandomPoint(ourHabitat)
+    val dest4 = getLegalRandomPoint(ourHabitat)
+    val destinations = Set(dest1, dest2, dest3, dest4)
+    val sm: ShiftManager = ShiftManager(ourHabitat,(tiger, dest1), (elephant, dest2), (dog, dest3), (cat, dest4))
+    var i = 0
+    while(sm.animals.count(a => destinations.contains(a.position)) != sm.animals.size){
+      i += 1
+      sm.walk()
+      sm.animals.foreach(animal => require(ourHabitat.areas.filterNot(a => a.areaType == Fertile).count(a => a.contains(animal.position)) == 0))
+    }
+    assert(sm.animals.count(a => destinations.contains(a.position)) == sm.animals.size)
+  }
 
 
 
