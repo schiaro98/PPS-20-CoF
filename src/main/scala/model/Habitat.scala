d4d4ea95c0e5d01e2757d11192153e089d68f910
lucarossi147 <lucarossi147@studio.unibo.it>
2021-10-14 15:43:26 +0200

    added some doc

--- src/main/scala/model/Habitat.scala
+++ src/main/scala/model/Habitat.scala
@@ -54,32 +54,38 @@ object Habitat {
 
   def apply(unexpectedEvents: Probability = Probability(0),
             dimensions: (Int, Int) = Constants.MainMapDimension,
-            areas: Seq[Area] = Serializer(OfArea).deserializeManyFromFile(Constants.MainMap)(classOf[Area])
+            areas: Seq[Area] = Serializer(OfArea).deserializeManyFromFile(Constants.MainMap)(classOf[Area]),
            ): Habitat = SimpleHabitat(unexpectedEvents, dimensions, areas)
 
   def apply(habitatType: HabitatType,
             unexpectedEvents: Probability,
             dimensions: (Int, Int),
-            areas: Seq[Area]): Habitat = habitatType match {
-    case EmptyHabitatType => SimpleHabitat(unexpectedEvents, dimensions, Seq.empty)
+            areas: Seq[Area] = Seq.empty,
+           ): Habitat = habitatType match {
+    case EmptyHabitatType => SimpleHabitat(unexpectedEvents, dimensions, areas)
     case SimpleHabitatType => SimpleHabitat(unexpectedEvents, dimensions, areas)
     case RandomHabitatType => SimpleHabitat(unexpectedEvents, dimensions, createRandomAreas(dimensions))
     case GridHabitatType => SimpleHabitat(unexpectedEvents, dimensions, createGridArea(dimensions, DefaultGridSize))
     case _ => throw new IllegalArgumentException("Habitat type error on method apply")
   }
+  
+  /**
+   * 
+   * @return a random AreaType
+   */
+  private def randomAreaType: AreaType = {
+    val choices: Seq[AreaType] = Seq(Fertile, Water, Rock, Volcano)
+    Random.shuffle(choices).head
+  }
 
-  private case class SimpleHabitat(override val unexpectedEvents: Probability,
-                                   override val dimensions: (Int, Int),
-                                   override val areas: Seq[Area]) extends Habitat
-
-
+  // TODO: Refactor createRandomAreas 
   /**
    * This method create an habitat, of given dimension, with diven areas, arranged in a randow way
    *
    * @param dimension dimension of the habitat
    * @return
    */
-  def createRandomAreas(dimension: (Int, Int)): Seq[Area] = {
+  private def createRandomAreas(dimension: (Int, Int)): Seq[Area] = {
     createRandomAreas(dimension, 4)
   }
 
@@ -90,11 +96,11 @@ object Habitat {
    * @param numberOfAreas Number of areas in the habitat
    * @return
    */
-  def createRandomAreas(dimension: (Int, Int), numberOfAreas: Int): Seq[Area] = {
+  private def createRandomAreas(dimension: (Int, Int), numberOfAreas: Int): Seq[Area] = {
     var grid = List[Area]()
     require(dimension._1 * dimension._2 > numberOfAreas * 10)
     val r = RectangleArea(Point(0, 0), Point(1, 1))
-    r.getIn4Quadrant(dimension).foreach(rectangle => grid = grid.::(Area(Area.randomType, rectangle, Probability(Random.between(1, 100)))))
+    r.getIn4Quadrant(dimension).foreach(rectangle => grid = grid.::(Area(randomAreaType, rectangle, Probability(Random.between(1, 100)))))
     grid
   }
 
@@ -105,7 +111,7 @@ object Habitat {
    * @param numberOfAreas num of areas in the grid
    * @return
    */
-  def createGridArea(dimension: (Int, Int), numberOfAreas: Int): Seq[Area] = {
+  private def createGridArea(dimension: (Int, Int), numberOfAreas: Int): Seq[Area] = {
     var grid = List[Area]()
     require(dimension._1 * dimension._2 > numberOfAreas * 10)
 
@@ -118,11 +124,22 @@ object Habitat {
         val areaWidth = Random.between(maxWidth / 2, maxWidth)
         val areaHeight = Random.between(maxHeight / 2, maxHeight)
         val rectangle = RectangleArea(startingPoint, Point(startingPoint.x + areaWidth, startingPoint.y + areaHeight))
-        val newArea: Area = Area(Area.randomType, rectangle, Probability(Random.between(1, 100)))
+        val newArea: Area = Area(randomAreaType, rectangle, Probability(Random.between(1, 100)))
         grid = grid.::(newArea)
       }
     }
     grid
   }
 
+  /**
+   * A simple implementation of an habitat
+   * @param unexpectedEvents a probability of which some unexpected event could occur
+   * @param dimensions the dimensions of the area
+   * @param areas the areas inside the Habitat
+   */
+  private case class SimpleHabitat(override val unexpectedEvents: Probability,
+                                   override val dimensions: (Int, Int),
+                                   override val areas: Seq[Area],
+                                  ) extends Habitat
+  
 }
