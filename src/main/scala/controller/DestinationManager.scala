eef37452204832a4e24bd1ae7f8396c467855887
schiaro98 <davide.schiaroli2@studio.unibo.it>
2021-10-18 12:55:27 +0200

    Added scaladoc

--- src/main/scala/controller/DestinationManager.scala
+++ src/main/scala/controller/DestinationManager.scala
@@ -7,9 +7,9 @@ import scala.annotation.tailrec
 
 sealed trait DestinationManager {
   /**
-   * For any given animals, calculate the target of the movement
-   * For an herbivore, it should point to the more near vegetables or a random location if it can't see anything
-   * For a Carnivore, it should point to the more near herbivore animal, or a random location
+   * For any given [[Animal]], calculate the target of the movement
+   * For an [[Herbivore]], it should point to the more near vegetables or a random location if it can't see anything
+   * For a [[Carnivore]], it should point to the more near herbivore animal, or a random location
    * @return
    */
   def calculateDestination() : Map[Animal, Point]
@@ -45,6 +45,13 @@ private case class DestinationManagerImpl(animals: Seq[Animal], food: Seq[FoodIn
     destination
   }
 
+  /**
+   * Find the nearest resource
+   * @param animal animal target of the control
+   * @param resources seq to be checked for find the nearest
+   * @tparam P describe a Placeable that can be an animal or a foodIstance
+   * @return
+   */
    def findNearestResource[P <: Placeable](animal: Animal, resources: Seq[P]): Option[Point] = {
      resources
        .map(resource => resource.position)
@@ -52,12 +59,24 @@ private case class DestinationManagerImpl(animals: Seq[Animal], food: Seq[FoodIn
        .minByOption(point => point.distance(animal.position))
    }
 
+  /**
+   * Find a random legal point inside the habitat
+   * Legal point are point not inside not walkable areas
+   * @param h the habitat
+   * @return the random point
+   */
   @tailrec
   final def getLegalRandomPoint(h: Habitat): Point = {
     val p = Point.getRandomPoint(h.dimensions)
     if (h.areas.filterNot(a => a.areaType == Fertile).count(a => a.contains(p)) == 0) p else getLegalRandomPoint(h)
   }
 
+  /**
+   * Find the nearest water zone
+   * @param animal that need water zone
+   * @param h the habitat
+   * @return a random point inside the water zone
+   */
   def findNearestWaterZone(animal:Animal, h: Habitat) : Option[Point] = {
     h.areas
       .filter(area => area.areaType == Water)
