c10b257976a486ddf91f04c609b8d8ec0e395a8b
lucarossi147 <lucarossi147@studio.unibo.it>
2021-10-20 00:53:43 +0200

    Created Statistics GUI

--- build.sbt
+++ build.sbt
@@ -13,8 +13,11 @@ libraryDependencies += "com.google.code.gson" % "gson" % "2.8.8"
 libraryDependencies += "org.scalactic" %% "scalactic" % "3.2.9"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.9" % "test"
 
-//scala concurrency
+//scala parallelism
 // https://mvnrepository.com/artifact/org.scala-lang.modules/scala-parallel-collections
 libraryDependencies += "org.scala-lang.modules" %% "scala-parallel-collections" % "1.0.4"
+
+// https://mvnrepository.com/artifact/jfree/jfreechart
+libraryDependencies += "jfree" % "jfreechart" % "1.0.13"
 
 scalacOptions += "-deprecation"
--- src/main/scala/utility/Statistics.scala
+++ src/main/scala/utility/Statistics.scala
@@ -14,22 +14,17 @@ object Statistics {
    * @param deathInBattle to sum
    * @param deathForNaturalCause to sum
    */
-  def update(foods: Int = 0,
-             attacks: Int = 0,
-             foodEaten: Int = 0,
-             deathInBattle: Int = 0,
-             deathForNaturalCause: Int = 0,
-            ): Unit = map =
+  def update(foods: Double = 0, attacks: Double = 0, foodEaten: Double = 0, deathInBattle: Double = 0, deathForNaturalCause: Double = 0): Unit = map =
     if (map isDefinedAt time)
       map + (time -> map(time).sum(StatisticsForTimeT(foods, attacks, foodEaten, deathInBattle, deathForNaturalCause)))
     else
       map + (time -> StatisticsForTimeT(foods, attacks, foodEaten, deathInBattle, deathForNaturalCause))
 
-  protected case class StatisticsForTimeT(foods: Int = 0,
-                                          attacks: Int = 0,
-                                          foodEaten: Int = 0,
-                                          deathInBattle: Int = 0,
-                                          deathForNaturalCause: Int = 0,
+  protected case class StatisticsForTimeT(foods: Double = 0,
+                                          attacks: Double = 0,
+                                          foodEaten: Double = 0,
+                                          deathInBattle: Double = 0,
+                                          deathForNaturalCause: Double = 0,
                                          ) {
     def sum(s: StatisticsForTimeT): StatisticsForTimeT =
       StatisticsForTimeT(foods + s.foods, attacks + s.attacks, foodEaten + s.foodEaten,
