b732596f20423ecd756a96bf4fb0a3d870da87e5
lucarossi147 <lucarossi147@studio.unibo.it>
2021-10-20 16:51:04 +0200

    minor fixes on Statistics

--- src/test/scala/utility/StatisticsTest.scala
+++ src/test/scala/utility/StatisticsTest.scala
@@ -7,22 +7,24 @@ class StatisticsTest extends AnyFunSuite{
   //We are testing a singleton so run all tests together or they won't pass
 
   test("Test add first stats"){
+    Statistics.restart()
     Statistics.update(deathForNaturalCause = 1)
     Statistics.update(0,0,0,0,41)
     assert(Statistics.map(0).deathForNaturalCause == 42)
   }
 
   test("test add time 1"){
+    Statistics.restart()
     Statistics.incTime()
     Statistics.update(foods = 1)
-    assert(Statistics.map(Statistics.time()).foods == 1)
+    assert(Statistics.map(Statistics.time).foods == 1)
   }
 
   test("Adding others value in time 1"){
     Statistics.update(foods = 1)
     Statistics.update(foodEaten = 1)
-    assert(Statistics.map(Statistics.time()).foods == 2 )
-    assert(Statistics.map(Statistics.time()).foodEaten == 1 )
+    assert(Statistics.map(Statistics.time).foods == 2 )
+    assert(Statistics.map(Statistics.time).foodEaten == 1 )
   }
 
 }
